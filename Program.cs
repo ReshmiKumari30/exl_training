8cced1de075e3350fd37ea9f7273f3eb2b7aa8c8
Reshmi Kumari <76199263+ReshmiKumari30@users.noreply.github.com>
2023-08-02 12:22:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Class5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new string[] { "flower", "axe", "mango", "owl", "yak" };
+            var result = words.Where(  element => element.Contains("a"));
+
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);    
+            }
+        }
+    }
+}
